91ad3f2c1a5ac69ae8fd676b98619f3b2c39ac32
Lukasz Hoszowski <hoszak@gmail.com>
2022-01-29 18:59:10 +0100

    BGA_002_Django_Secret_Key

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l4d1@$t_@)sax63^d7hfq_^4v)btqy#=*im(j#@ak^6xq54wg)'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
