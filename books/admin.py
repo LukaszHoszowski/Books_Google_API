12c24d54916bd6a9b359e90e158a1814bbafabb2
Lukasz Hoszowski <hoszak@gmail.com>
2022-01-29 18:29:25 +0100

    BGA_002_Init

--- books/admin.py
+++ books/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Book)
+admin.site.register(models.Author)
--- books/models.py
+++ books/models.py
@@ -3,15 +3,25 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    published_date = models.DateTimeField()
+    published_date = models.SmallIntegerField()
     isbn = models.CharField(max_length=13)
     page_count = models.IntegerField(help_text='Page count')
     cover_link = models.CharField(max_length=300)
     language = models.CharField(max_length=50)
+    author = models.ManyToManyField('Author', related_name="authors")
 
     def __str__(self):
         return f'{self.title}'
 
     class Meta:
         ordering = ['title']
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        ordering = ['name']
