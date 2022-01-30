e896554ef79eafc32bd7c7de018d2b93b4f19039
Lukasz Hoszowski <hoszak@gmail.com>
2022-01-30 10:58:46 +0100

    BGA_002_Models_Help_texts

--- books/models.py
+++ books/models.py
@@ -4,13 +4,13 @@ from .validators import validate_year, validate_isbn
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    published_date = models.SmallIntegerField(validators=[validate_year])
-    isbn = models.CharField(max_length=13, validators=[validate_isbn])
-    page_count = models.PositiveIntegerField(help_text='Page count')
-    cover_link = models.URLField()
-    language = models.ForeignKey('Language', on_delete=models.DO_NOTHING)
-    author = models.ManyToManyField('Author', related_name="authors")
+    title = models.CharField(max_length=200, help_text='Title')
+    published_date = models.SmallIntegerField(null=True, blank=True, validators=[validate_year], help_text='Publish year')
+    isbn = models.CharField(max_length=13, null=True, blank=True, validators=[validate_isbn], help_text='ISBN')
+    page_count = models.PositiveIntegerField(null=True, blank=True, help_text='Page count')
+    cover_link = models.URLField(null=True, blank=True, help_text='Link to cover of the book')
+    language = models.ForeignKey('Language', on_delete=models.DO_NOTHING, help_text='Book language')
+    author = models.ManyToManyField('Author', related_name="authors", help_text='Authors list')
 
     def __str__(self):
         return f'{self.title}'
@@ -20,7 +20,7 @@ class Book(models.Model):
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, help_text='Author name')
 
     def __str__(self):
         return f'{self.name}'
@@ -30,7 +30,7 @@ class Author(models.Model):
 
 
 class Language(models.Model):
-    lang = models.CharField(max_length=200)
+    lang = models.CharField(max_length=200, help_text='Language')
 
     def __str__(self):
         return f'{self.lang}'
