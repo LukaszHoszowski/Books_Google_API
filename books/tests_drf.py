d12fd142d38c12bef34299e5691ec9fb2a4c86e2
Lukasz Hoszowski <hoszak@gmail.com>
2022-01-31 23:47:54 +0100

    BGA_007_DRF_test

---
+++ books/tests_drf.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+from .models import Book, Author, Language
+
+
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        language = Language.objects.create(lang='en')
+        author = Author.objects.create(name='Jan Kowalski')
+        book = Book.objects.create(title='costam',
+                                   published_date=1999,
+                                   isbn='1111222233334',
+                                   page_count=100,
+                                   cover_link='http://books.google.com/books/content?id=mnkbAQAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+                                   language=language)
+        book.author.add(author)
+
+    def test_post_content(self):
+        book = Book.objects.get(pk=1)
+        self.assertEqual(book.title, 'costam')
+        self.assertEqual(book.published_date, 1999)
+        self.assertEqual(book.isbn, '1111222233334')
+        self.assertEqual(book.page_count, 100)
+        self.assertEqual(book.cover_link, 'http://books.google.com/books/content?id=mnkbAQAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api')
+        self.assertEqual(book.language.lang, 'en')
+        self.assertEqual(book.author.name, 'Jan Kowalski')
