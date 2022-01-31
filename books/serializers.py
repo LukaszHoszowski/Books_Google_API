ac58143ed34da7c7471e46b276aa9410142db247
Lukasz Hoszowski <hoszak@gmail.com>
2022-01-31 19:36:28 +0100

    BGA_005_ViewSet/Router

--- books/serializers.py
+++ books/serializers.py
@@ -4,10 +4,11 @@ from .models import Book
 
 
 class BookSerializer(serializers.ModelSerializer):
-    language = serializers.StringRelatedField(many=False, read_only=True)
+    language = serializers.StringRelatedField(many=False, read_only=False)
+    author = serializers.StringRelatedField(many=True, read_only=False)
     # author = serializers.ManyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Book
         fields = '__all__'
-        depth = 1
+        depth = 0
