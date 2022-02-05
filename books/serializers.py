549ad94c7b99c4b527a4abee3135018fb962b882
Lukasz Hoszowski <hoszak@gmail.com>
2022-02-05 01:37:51 +0100

    BGA_008_Docker_optimized

--- books/serializers.py
+++ books/serializers.py
@@ -6,7 +6,6 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     language = serializers.StringRelatedField(many=False, read_only=False)
     author = serializers.StringRelatedField(many=True, read_only=False)
-    # author = serializers.ManyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Book
